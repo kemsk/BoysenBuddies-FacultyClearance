eb225106e1f371b3c33b45de8d8bbb60c10ba8e2
Albert Floyd Villanueva <20220025546@my.xu.edu.ph>
2026-03-01 17:18:54 +0800

    fixed dashboard routing

--- FC/views.py
+++ FC/views.py
@@ -406,7 +406,6 @@ def google_oauth_callback(request):
     print(f"GOOGLE OAUTH: About to call django_login...")
     
     django_login(request, user, backend="django.contrib.auth.backends.ModelBackend")
-    return HttpResponseRedirect("/login-prompt")
     
     print(f"GOOGLE OAUTH: Session after django_login: {dict(request.session)}")
     print(f"GOOGLE OAUTH: Session key after: {request.session.session_key}")
