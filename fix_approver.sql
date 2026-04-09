238cfb4e4b4fd6f42f591f5bca140618a71d4805
Albert Floyd Villanueva <20220025546@my.xu.edu.ph>
2026-04-09 15:19:30 +0800

    removed python and sql commands

--- fix_approver.sql
+++
@@ -1,4 +0,0 @@
-UPDATE FC_approver 
-SET approver_type = 'Department', 
-    department_id = (SELECT id FROM FC_department WHERE abbreviation = 'IT' AND college_id = (SELECT id FROM FC_college WHERE abbreviation = 'CCS'))
-WHERE user_id = (SELECT id FROM FC_user WHERE email = '201131134@my.xu.edu.ph');
